77d4fdc031f8c8e6e6360c35cf22556561de9c34
dsantelmo <dsantelmo@gmail.com>
2013-06-08 00:43:19 +0200

    quick & dirty note taking for sync tests

---
+++ note/note.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import uuid
+import sys
+
+db_path='_notes.sqlite'
+
+def __usage_exit():
+    print "USAGE" #FIXME
+    sys.exit(1)
+
+def __init_db(con):
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS notes
+      (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+       uuid TEXT,
+       text TEXT,
+       action TEXT)""")
+    return cur
+
+#FIXME ugly, redundant code...
+#FIXME abstract database access
+def cli_create():
+    if len(sys.argv) < 3:
+        __usage_exit()
+    note = ' '.join(sys.argv[2:])
+    with sqlite3.connect(db_path) as con:
+        cur = __init_db(con)
+        inserts = (note, uuid.uuid4().hex, 'NEW')
+        cur.execute("""INSERT OR IGNORE INTO notes
+                       (text, uuid, action) VALUES (?,?,?)""", inserts)
+        cur.execute('SELECT last_insert_rowid()')
+        note_id = cur.fetchone()
+        con.commit()
+        #FIXME control SQLITE_FULL
+        cur.execute("SELECT * FROM notes WHERE id = ?", note_id)
+        print (cur.fetchone())
+
+def cli_modify():
+    if len(sys.argv) < 4:
+        __usage_exit()
+    note_uuid = sys.argv[2]
+    note_text = ' '.join(sys.argv[3:])
+    with sqlite3.connect(db_path) as con:
+        cur = __init_db(con)
+        cur.execute("""SELECT id, action FROM notes WHERE uuid = ?
+                       ORDER BY id DESC LIMIT 1""", (note_uuid,))
+        note = cur.fetchone()
+        action = 'MOD'
+        if note is None:
+            print "That note doesn't exist."
+            action = 'ERROR'
+        elif note[1] == 'DEL':
+            print "That note was deleted. Re-creating"
+        if action != 'ERROR':
+            insert = (note_text, note_uuid, action)
+            cur.execute("""INSERT INTO notes (text, uuid, action)
+                           VALUES (?,?,?)""", insert)
+            con.commit()
+
+def cli_delete():
+    if len(sys.argv) < 3:
+        __usage_exit()
+    note_uuid = sys.argv[2]
+    with sqlite3.connect(db_path) as con:
+        cur = __init_db(con)
+        cur.execute("""SELECT id, action FROM notes WHERE uuid = ?
+                       ORDER BY id DESC LIMIT 1""", (note_uuid,))
+        note = cur.fetchone()
+        if note is None or note[1] == 'DEL':
+            print "ERROR: that note didn't exist."
+            sys.exit(1) #FIXME
+        insert = ('', note_uuid, 'DEL')
+        cur.execute("""INSERT INTO notes (text, uuid, action)
+                       VALUES (?,?,?)""", insert)
+        con.commit()
+
+
+def cli_list():
+    with sqlite3.connect(db_path) as con:
+        cur = __init_db(con)
+        cur.execute("""SELECT a.uuid, a.text FROM notes a
+                       INNER JOIN (SELECT id, uuid FROM notes
+                                   GROUP BY uuid ORDER BY id DESC) b
+                       ON a.id = b.id WHERE a.action <> 'DEL'""")
+        notes = cur.fetchall() #FIXME paginate?
+        for note in notes:
+            print note[0] + ": " + note[1]
+
+try:
+    cli_options = {
+      'create' : cli_create,
+      'modify' : cli_modify,
+      'delete' : cli_delete,
+      'list' : cli_list,
+    }
+    cli_options[sys.argv[1]]()
+    sys.exit(0)
+except KeyError:
+    __usage_exit()
+
