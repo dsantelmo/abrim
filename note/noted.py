c7485c5dd57b44dfbd97099f50cff9abe62feb72
dsantelmo <dsantelmo@gmail.com>
2013-06-08 14:47:55 +0200

    initial Flask module for HTTP sync

---
+++ note/noted.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sqlite3
+from note import *
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def __root():
+    return redirect(url_for('last', changes_num=1))
+
+@app.route('/last/<int:changes_num>')
+def __last(changes_num):
+    return last(changes_num)
+
+@app.route('/changes_since_id/<change_id>')
+def __changes_since_id(change_id):
+    return changes_since_id(change_id)
+
+@app.route('/summ_changes_since_id/<change_id>')
+def __summ_changes_since_id(change_id):
+    return summ_changes_since_id(change_id)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
