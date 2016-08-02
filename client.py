cd82c0a27ac665f639cbaf37ddfde842747ec03b
dsantelmo <dsantelmo@gmail.com>
2016-08-02 16:18:20 +0200

    server shadows creation

--- client.py
+++ client.py
@@ -51,27 +51,61 @@ temp_client_file_name = os.path.join( tempfile.gettempdir(),
 def show_main_form():
     main_form = ""
     main_form1 = """
-<form action="/sync" method="post">
-text:
-<textarea name="client_text">"""
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Title of the document</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    </head>
+
+  <body>
+    <header>
+        <h1>header</h1>
+    </<header>
+    <nav>main nav</nav>
+    <main>
+        <article>
+            <header>
+              <h1>text title</h1>
+              <p>Last modified: <time datetime="2000-01-01T00:00:00Z">on 2000/01/01 at 0:00pm</time>
+              </p>
+            </header>
+            <section>
+                <form action="/sync" method="post">
+                    <p>
+                        <textarea name="client_text" placeholder="Some text here...">"""
     main_form2 = """</textarea>
-<br /><br />
-shadow
-<textarea disabled>
+                    </p>
+                    <p>
+                        <input type="submit" value="Sync!">
+                    </p>
+                </form>
+            </section>
+            <section>
+                <form>
+                  <textarea name="client_shadow" disabled placeholder="Shadow text...">
 """
 
     main_form3 = """</textarea>
-<br /><br />
-<input type="submit" value="Sync!">
-</form>
+                </form>
+            </section>
+        </article>
+    </main>
+    <nav>recent nav</nav>
+    <nav>tags nav<nav>
+    <footer>footer</footer>
+  </body>
+</html>
 """
     with closing(shelve.open(temp_client_file_name)) as d:
         if not 'client_text' in d:
             d['client_text'] = """Bad dog. KO"""
         if not 'client_shadow' in d:
             d['client_shadow'] = """Bad dog. KO"""
-        main_form = "In " + temp_client_file_name + " <br /><br />" + \
-                    main_form1 + d['client_text'] + \
+        main_form = main_form1 + d['client_text'] + \
                     main_form2 + d['client_shadow'] + \
                     main_form3
     return main_form 
@@ -86,17 +120,19 @@ import sys
 
 def show_sync(client_text):
     client_shadow = None
-    
+
     with closing(shelve.open(temp_client_file_name)) as d:
         try:
             client_shadow =  d['client_shadow']
         except KeyError, e:
+            print("show_sync 500 A")
             abort(500)
 
         if not client_text or not client_shadow:
+            print("show_sync 500 B")
             abort(500)
-            
-        
+
+
         diff_obj = diff_match_patch.diff_match_patch()
         diff_obj.Diff_Timeout = DIFF_TIMEOUT
 
@@ -118,35 +154,60 @@ def show_sync(client_text):
             print ("step 2 results: {}".format(text_patches))
 
             try:
-                #
                 #step 3
                 #
                 # Client Text is copied over to Shadow. This copy must be identical to
                 # the value of Client Text in step 1, so in a multi-threaded environment
                 # a snapshot of the text should have been taken.
-                #
+
                 client_shadow_cksum =  hashlib.md5(client_shadow).hexdigest()
                 # FIXME what happens on first sync?
                 print("client_shadow_cksum {}".format(client_shadow_cksum))
-                #
+
                 d['client_shadow'] = client_text
 
-                url = "http://127.0.0.1:5002/send_sync"
-                payload = {
-                           'client_id': CLIENT_ID,
-                           'client_shadow_cksum': client_shadow_cksum,
-                           'client_patches': text_patches,
-                          }
-                print(payload)
-                ok = requests.post(
-                  url,
-                  headers={'Content-Type': 'application/json'},
-                  data=json.dumps(payload)
-                  )
-                #json.loads(urllib2.urlopen(url + '/send_sync').read())
 
-                print ok
-                return "ok"
+                # send text_patches, client_id and client_shadow_cksum
+
+                url = "http://127.0.0.1:5002/send_sync"
+
+                r = send_sync(url, CLIENT_ID, client_shadow_cksum, text_patches)
+
+                print("-------------------")
+                try:
+                    r_json = r.json()
+                    if 'status' in r_json:
+                        if not r_json['status'] == u"OK":
+                            error_return = "Unknown error in response"
+                            if 'error_type' in r_json:
+                                if r_json['error_type'] == u"NoServerShadow":
+                                    # client sends its shadow:
+                                    r_shadow = send_shadow("http://127.0.0.1:5002/send_shadow", CLIENT_ID, d['client_shadow'])
+                                    try:
+                                        r_shadow_json = r_shadow.json()
+                                        if 'status' in r_shadow_json:
+                                            if r_shadow_json['status'] == "OK":
+                                                return "Shadow updated. Sync Again"
+                                            else:
+                                                return "ERROR: unable to send_shadow"
+                                        else:
+                                            return "ERROR: send_shadow response doesn't contain status"
+                                    except ValueError, e:
+                                        return(r_shadow.text)
+                                else:
+                                    error_return = "ERROR<br />" + r_json['error_type']
+                                    if  'error_message' in r_json:
+                                        error_return = error_return + "<br />" + r_json['error_message']
+                            return error_return
+                        else:
+                            print("sync seems to be going OK")
+                            print("FIXME: CONTINUE HERE")
+                            return("sync OK")
+                    else:
+                        return "ERROR: send_sync response doesn't contain status"
+                    {u'status': u'ERROR', u'error_type': u'NoServerShadow', u'error_message': u'No shadow found in the server. Send it again'}
+                except ValueError, e:
+                    return(r.text)
             except ValueError:
                 print "ValueError"
                 return "ERROR: ValueError" #FIXME
@@ -157,8 +218,34 @@ def show_sync(client_text):
                 print "ConnectionError"
                 return "ERROR: ConnectionError" #FIXME
 
+
+    print("show_sync 500 C")
     abort(500)
 
+def send_sync(url, client_id, client_shadow_cksum, client_patches):
+    payload = {
+               'client_id': client_id,
+               'client_shadow_cksum': client_shadow_cksum,
+               'client_patches': client_patches,
+              }
+
+    return requests.post(
+      url,
+      headers={'Content-Type': 'application/json'},
+      data=json.dumps(payload)
+      )
+
+def send_shadow(url, client_id, client_shadow):
+    payload = {
+               'client_id': client_id,
+               'client_shadow': client_shadow,
+              }
+
+    return requests.post(
+      url,
+      headers={'Content-Type': 'application/json'},
+      data=json.dumps(payload)
+      )
 
 if __name__ == "__main__":
     app.run(port=5001)
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, redirect, url_for, abort
+from flask import request, redirect, url_for, abort
+import flask
 import diff_match_patch
 
 DIFF_TIMEOUT=0.1
@@ -13,19 +14,10 @@ app = Flask(__name__)
 app.debug = True
 
 
-
-# return flask.jsonify(**f)
-#    return jsonify(username=g.user.username,
-#                   email=g.user.email,
-#                   id=g.user.id)
-#
-
-
 #@app.route('/', methods=['POST',])
 @app.route('/', methods=['GET', 'POST'])
 def __root():
     return show_main_form()
-
 
 
 @app.route('/send_sync', methods=['POST'])
@@ -34,6 +26,15 @@ def __sync():
         abort(404)
     else:
         return send_sync(request)
+
+
+@app.route('/send_shadow', methods=['POST'])
+def __shadow():
+    if request.method != 'POST':
+        abort(404)
+    else:
+        return send_shadow(request)
+
 
 
 from contextlib import closing
@@ -46,7 +47,7 @@ import os
 
 SERVER_ID='server1'
 
-temp_client_file_name = os.path.join( tempfile.gettempdir(),
+temp_server_file_name = os.path.join( tempfile.gettempdir(),
   tempfile.gettempprefix() + SERVER_ID)
 
 
@@ -61,18 +62,166 @@ import requests
 import json
 
 def send_sync(request):
-    r = request.json
-    print r
-    if r:
-        print r['client_id']
-        print r['client_shadow_cksum']
-        print r['client_patches']
-        return_print = r['client_id'] + "<br />"
-        return_print = return_print + r['client_shadow_cksum'] + "<br />"
-        return_print = return_print + r['client_patches'] + "<br />"
-        return return_print
+    req = request.json
+    res = None
+    if req and 'client_id' in req and 'client_shadow_cksum' in req and 'client_patches' in req:
 
-    abort(500)
+        # steps 4 (atomic with 5, 6 & 7)
+        #
+        # The edits are applied to Server Text on a best-effort basis
+        # Server Text is updated with the result of the patch. Steps 4 and 5
+        # must be atomic, but they do not have to be blocking; they may be
+        # repeated until Server Text stays still long enough.
+        #
+        # Client Text and Server Shadow (or symmetrically Server Text and
+        # Client Shadow) must be absolutely identical after every half of the
+        # synchronization
+        #
+        # receive text_patches and client_shadow_cksum
+        #
+        server_text = None
+        server_shadows = None
+        with closing(shelve.open(temp_server_file_name)) as d:
+            if not 'server_text' in d:
+                d['server_text'] = ""
+            if not 'server_shadows' in d:
+                d['server_shadows'] = {}
+            server_text = d['server_text']
+            server_shadows = d['server_shadows']
+        
+        # first check the server shadow cheksum
+        # if server_shadows[client_id] is empty ask for it
+        
+        client_id = req['client_id']
+        
+        if not client_id in server_shadows:
+            res = err_response('NoServerShadow',
+            'No shadow found in the server. Send it again')
+        else:
+
+            print("FIXME: CONTINUE HERE")
+        
+        #    print("Shadow received. Now you can sync!")
+        #    server_shadows[client_id] = client_shadow
+        #else:
+        #    server_shadow_cksum = hashlib.md5(
+        #      server_shadows[client_id]).hexdigest()
+        #    print("server_shadow_cksum {}".format(server_shadow_cksum))
+        #    if client_shadow_cksum != server_shadow_cksum:
+        #        #FIXME what happenson first sync?
+        #        print("too bad! Shadows got desynced. "
+        #              "I'm sending back ALLserver shadow text, "
+        #              "use it a your client shadow")
+        #        print(server_shadows[client_id])
+        #        #clients updates its shadow AND text:
+        #        print("DATALOSS on latest client text. "
+        #          "Updating with server text")
+        #        client_shadow = server_shadows[client_id]
+        #        client_text = client_shadow
+        #
+        #
+
+
+        res = {
+            'status': 'OK',
+            }
+    else:
+        if not req:
+            res = err_response('NoPayload', 
+            'No payload found in the request')
+        elif not 'client_id' in req:
+            res = err_response('PayloadMissingAttribute', 
+            'No client_id found in the request')
+        elif not 'client_shadow_cksum' in req:
+            res = err_response('PayloadMissingAttribute', 
+            'No client_shadow_cksum found in the request')
+        elif not 'client_patches' in req:
+            res = err_response('PayloadMissingAttribute', 
+            'No client_patches found in the request')
+        else:
+            print("send_sync 500")
+            abort(500)
+    print("response:")
+    print(res)
+    return flask.jsonify(**res)
+
+
+
+
+
+
+def send_shadow(request):
+    req = request.json
+    res = None
+    if req and 'client_id' in req and 'client_shadow' in req:
+
+        # steps 4 (atomic with 5, 6 & 7)
+        #
+        # The edits are applied to Server Text on a best-effort basis
+        # Server Text is updated with the result of the patch. Steps 4 and 5
+        # must be atomic, but they do not have to be blocking; they may be
+        # repeated until Server Text stays still long enough.
+        #
+        # Client Text and Server Shadow (or symmetrically Server Text and
+        # Client Shadow) must be absolutely identical after every half of the
+        # synchronization
+        #
+        # receive text_patches and client_shadow_cksum
+        #
+        server_shadows = None
+        
+        res = err_response('UnknowErrorSendShadow',
+        'Unknown error in send_shadow')
+        
+        with closing(shelve.open(temp_server_file_name)) as d:
+            if not 'server_shadows' in d:
+                d['server_shadows'] = {}
+            server_shadows = d['server_shadows']
+        
+            # first check the server shadow cheksum
+            # if server_shadows[client_id] is empty ask for it
+            
+            print("saving shadow from " + req['client_id'])
+            print("----")
+            print(req['client_shadow'])
+            print("----")
+            client_id = req['client_id']
+            server_shadows[client_id] = req['client_shadow']
+            d['server_shadows'] = server_shadows
+            
+            print d['server_shadows']
+            
+            res = {
+                'status': 'OK',
+                }
+
+    else:
+        if not req:
+            res = err_response('NoPayload',
+            'No payload found in the request')
+        elif not 'client_id' in req:
+            res = err_response('PayloadMissingAttribute',
+            'No client_id found in the request')
+        elif not 'client_shadow' in req:
+            res = err_response('PayloadMissingAttribute',
+            'No client_shadow found in the request')
+        else:
+            print("send_shadow 500")
+            abort(500)
+    print("response:")
+    print(res)
+    return flask.jsonify(**res)
+
+
+
+def err_response(error_type, error_message):
+    return {
+        'status': 'ERROR',
+        'error_type': error_type,
+        'error_message': error_message,
+        }
+
+
 
 
 if __name__ == "__main__":
