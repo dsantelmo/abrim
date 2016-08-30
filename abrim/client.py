84b2ce866c33920435c32827c134e90395891f58
dsantelmo <dsantelmo@gmail.com>
2016-08-30 05:54:38 +0200

    commented code cleanup

--- abrim/client.py
+++ abrim/client.py
@@ -5,14 +5,6 @@ import os
 import sys
 import json
 import argparse
-from passlib.hash import bcrypt
-#hash = bcrypt.encrypt(usersPassword, rounds=app.config['BCRYPT_ROUNDS'])
-## Validating a hash
-#if bcrypt.verify(usersPassword, hash):
-import shelve
-# FIXME Warning Because the shelve module is backed by pickle, it is insecure
-# to load a shelf from an untrusted source. Like with pickle, loading a shelf
-# can execute arbitrary code
 from flask import Flask, g, request, redirect, url_for, abort, render_template, flash
 import diff_match_patch
 import hashlib
@@ -108,17 +100,11 @@ def show_datastore_form():
     #    return __print_iter_contents(d, 6, temp_string)
     return "WIP"
 
-#def __print_iter_contents(iter_d, depth, temp_string):
-#    if depth > 0:
-#        for k, element in iter_d.items():
-#            if isinstance(element, dict):
-#                temp_string = temp_string + "<li><b>{0} :</b></li>".format(k)
-#                temp_string = temp_string + "<ul>"
-#                temp_string = __print_iter_contents(element, depth - 1, temp_string)
-#                temp_string = temp_string + "</ul>"
-#            else:
-#                temp_string = temp_string + "<li><b>{0}</b> : {1}</li>".format(k, element)
-#    return temp_string
+
+from passlib.hash import bcrypt
+#hash = bcrypt.encrypt(usersPassword, rounds=app.config['BCRYPT_ROUNDS'])
+## Validating a hash
+#if bcrypt.verify(usersPassword, hash):
 
 
 def show_main_form():
@@ -363,199 +349,6 @@ def send_sync(client_text, recursive_count):
     abort(500)
 
 
-#
-#
-#def get_sync(client_text, recursive_count):
-#
-#    recursive_count += 1
-#    if recursive_count > MAX_RECURSIVE_COUNT:
-#        return "MAX_RECURSIVE_COUNT"
-#
-#    client_shadow = None
-#    client_shadow = __get_client_attribute(CLIENT_ID, 'client_shadow')
-#
-#    url = "http://127.0.0.1:5002/get_sync"
-#    r = __get_sync_payload(url, CLIENT_ID)
-#    try:
-#        r_json = r.json()
-#        if 'status' in r_json:
-#            if not r_json['status'] == "OK":
-#                print("__manage_get_sync_error_return")
-#                error_return, new_client_shadow = __manage_get_sync_error_return(
-#                        r_json,
-#                        CLIENT_ID,
-#                        recursive_count
-#                )
-#                #__set_content_attribute(CLIENT_ID, 'client_text', client_text)
-#                #__set_content_attribute(CLIENT_ID, 'client_shadow', client_text)
-#                #if new_client_shadow:
-#                #    __set_content_attribute(CLIENT_ID, 'client_shadow', new_client_shadow)
-#                #    error_return = send_sync(__get_client_attribute(CLIENT_ID, 'client_text'), recursive_count)
-#                #return error_return
-#                return r_json['status']
-#            else:
-#                #__set_content_attribute(CLIENT_ID, 'client_text', client_text)
-#                #__set_content_attribute(CLIENT_ID, 'client_shadow', client_text)
-#                #print("sync seems to be going OK")
-#                #flash("Sync OK!", 'info')
-#                #print("FIXME: CONTINUE HERE")
-#                #return redirect(url_for('__main'), code=302)
-#                return "OK"
-#        else:
-#            return "ERROR: send_sync response doesn't contain status"
-#    except ValueError:
-#        return(r.text)
-#
-#    #FIXME: continue here
-#
-#    diff_obj = diff_match_patch.diff_match_patch()
-#    diff_obj.Diff_Timeout = DIFF_TIMEOUT
-#
-#    # from https://neil.fraser.name/writing/sync/
-#    # step 1 & 2
-#    # Client Text is diffed against Shadow. This returns a list of edits which
-#    # have been performed on Client Text
-#
-#    edits = None
-#    if not client_shadow:
-#        edits = diff_obj.diff_main("", client_text)
-#    else:
-#        edits = diff_obj.diff_main(client_shadow, client_text)
-#    diff_obj.diff_cleanupSemantic(edits) # FIXME: optional?
-#
-#    patches = diff_obj.patch_make(edits)
-#    text_patches = diff_obj.patch_toText(patches)
-#
-#    if not text_patches:
-#        # nothing to update!
-#        flash("no changes", 'warn')
-#        return redirect(url_for('__main'), code=302)
-#    else:
-#        #print("step 2 results: {}".format(text_patches))
-#
-#        try:
-#            #step 3
-#            #
-#            # Client Text is copied over to Shadow. This copy must be identical to
-#            # the value of Client Text in step 1, so in a multi-threaded environment
-#            # a snapshot of the text should have been taken.
-#
-#            client_shadow_cksum = 0
-#            if not client_shadow:
-#                print("client_shadow: None")
-#            else:
-#                #print("client_shadow: " + client_shadow)
-#                client_shadow_cksum =  hashlib.md5(client_shadow.encode('utf-8')).hexdigest()
-#            #print("_____________pre__cksum_______")
-#            #print(client_shadow_cksum)
-#
-#            __set_content_attribute(CLIENT_ID, 'client_text', client_text)
-#            __set_content_attribute(CLIENT_ID, 'client_shadow', client_shadow)
-#
-#            # send text_patches, client_id and client_shadow_cksum
-#            url = "http://127.0.0.1:5002/send_sync"
-#            r = __send_sync_payload(url, CLIENT_ID, client_shadow_cksum, text_patches)
-#            try:
-#                r_json = r.json()
-#                if 'status' in r_json:
-#                    if not r_json['status'] == "OK":
-#                        print("__manage_get_sync_error_return")
-#                        error_return, new_client_shadow = __manage_get_sync_error_return(r_json, CLIENT_ID, recursive_count)
-#                        __set_content_attribute(CLIENT_ID, 'client_text', client_text)
-#                        __set_content_attribute(CLIENT_ID, 'client_shadow', client_text)
-#                        if new_client_shadow:
-#                            __set_content_attribute(CLIENT_ID, 'client_shadow', new_client_shadow)
-#                            error_return = send_sync(__get_client_attribute(CLIENT_ID, 'client_text'), recursive_count)
-#                        return error_return
-#                    else:
-#                        __set_content_attribute(CLIENT_ID, 'client_text', client_text)
-#                        __set_content_attribute(CLIENT_ID, 'client_shadow', client_text)
-#                        print("sync seems to be going OK")
-#                        flash("Sync OK!", 'info')
-#                        print("FIXME: CONTINUE HERE")
-#                        return redirect(url_for('__main'), code=302)
-#                else:
-#                    return "ERROR: send_sync response doesn't contain status"
-#            except ValueError:
-#                return(r.text)
-#        except ValueError:
-#            print("ValueError")
-#            return "ERROR: ValueError" #FIXME
-#        except requests.exceptions.ConnectionError:
-#            print("ConnectionError")
-#            return "ERROR: ConnectionError" #FIXME
-#
-#    abort(500)
-#
-#
-#def __manage_get_sync_error_return(r_json, client_id, recursive_count):
-#    new_client_shadow = None
-#
-#    client_text = __get_client_attribute(client_id, 'client_text')
-#    if not client_text:
-#        raise Exception('There should be a client_text by now...')
-#
-#    client_shadow = __get_client_attribute(client_id, 'client_shadow')
-#    if not client_shadow:
-#        client_shadow = ""
-#
-#    error_return = "Unknown error in response"
-#
-#    if 'error_type' in r_json:
-#        #if r_json['error_type'] == "NoServerText":
-#        #    print("NoServerText")
-#        #    # client sends its text:
-#        #    r_send_text = __send_text_payload("http://127.0.0.1:5002/send_text", client_id, client_text, client_shadow)
-#        #    try:
-#        #        r_send_text_json = r_send_text.json()
-#        #        if 'status' in r_send_text_json:
-#        #            if r_send_text_json['status'] == "OK":
-#        #                print("Text updated from client. Trying to sync again")
-#        #                flash("Text updated from client. Trying to sync again...", 'info')
-#        #                error_return = send_sync(client_text, recursive_count)
-#        #            else:
-#        #                error_return = "ERROR: unable to send_text"
-#        #        else:
-#        #            error_return =  "ERROR: send_text response doesn't contain status"
-#        #    except ValueError:
-#        #        error_return = r_send_text.text
-#        #el
-#        if r_json['error_type'] == "NoServerShadow":
-#            print("NoServerShadow")
-#            # client sends its shadow:
-#            r_send_shadow = __send_shadow_payload("http://127.0.0.1:5002/send_shadow", client_id, client_shadow)
-#            try:
-#                r_send_shadow_json = r_send_shadow.json()
-#                if 'status' in r_send_shadow_json:
-#                    if r_send_shadow_json['status'] == "OK":
-#                        print("Shadow updated from client. Trying to sync again")
-#                        flash("Shadow updated from client. Trying to sync again...", 'info')
-#                        error_return =  send_sync(client_text, recursive_count)
-#                    else:
-#                        error_return = "ERROR: unable to send_shadow"
-#                else:
-#                    error_return = "ERROR: send_shadow response doesn't contain status"
-#            except ValueError:
-#                error_return = r_send_shadow.text
-#        elif r_json['error_type'] == "ServerShadowChecksumFailed":
-#            print("ServerShadowChecksumFailed")
-#            # server sends its shadow:
-#            if 'server_shadow' in r_json:
-#                new_client_shadow = r_json['server_shadow']
-#                print("Shadow updated from server. Trying to sync again")
-#                flash("Shadow updated from server. Trying to sync again...", 'error')
-#                error_return =  send_sync(client_text, recursive_count)
-#            else:
-#                error_return = "ERROR: unable to update shadow from server"
-#        else:
-#            error_return = "ERROR<br />" + r_json['error_type']
-#            if  'error_message' in r_json:
-#                error_return = error_return + "<br />" + r_json['error_message']
-#                error_return = error_return + "<br />" + "FULL MESSAGE:<br />" + json.dumps(r_json)
-#
-#    return error_return, new_client_shadow
-#
-
 def __manage_send_sync_error_return(r_json, client_id, recursive_count):
     new_client_shadow = None
 
@@ -570,24 +363,6 @@ def __manage_send_sync_error_return(r_json, client_id, recursive_count):
     error_return = "Unknown error in response"
 
     if 'error_type' in r_json:
-        #if r_json['error_type'] == "NoServerText":
-        #    print("NoServerText")
-        #    # client sends its text:
-        #    r_send_text = __send_text_payload("http://127.0.0.1:5002/send_text", client_id, client_text, client_shadow)
-        #    try:
-        #        r_send_text_json = r_send_text.json()
-        #        if 'status' in r_send_text_json:
-        #            if r_send_text_json['status'] == "OK":
-        #                print("Text updated from client. Trying to sync again")
-        #                flash("Text updated from client. Trying to sync again...", 'info')
-        #                error_return = send_sync(client_text, recursive_count)
-        #            else:
-        #                error_return = "ERROR: unable to send_text"
-        #        else:
-        #            error_return =  "ERROR: send_text response doesn't contain status"
-        #    except ValueError:
-        #        error_return = r_send_text.text
-        #el
         if r_json['error_type'] == "NoServerShadow":
             print("NoServerShadow")
             # client sends its shadow:
@@ -639,20 +414,6 @@ def __send_sync_payload(url, client_id, client_shadow_cksum, client_patches):
       data=json.dumps(payload)
       )
 
-#def __send_text_payload(url, client_id, client_text, client_shadow):
-#    payload = {
-#               'client_id': client_id,
-#               'client_text': client_text,
-#               'client_shadow': client_shadow,
-#              }
-#
-#    print("__send_text_payload: " + \
-#            ''.join('{}{}'.format(key, val) for key, val in payload.items()))
-#    return requests.post(
-#      url,
-#      headers={'Content-Type': 'application/json'},
-#      data=json.dumps(payload)
-#      )
 
 def __send_shadow_payload(url, client_id, client_shadow):
     payload = {
