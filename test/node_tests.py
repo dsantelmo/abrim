a5e02c1ebb9a2a369e26bdba1ba708fbfb6d8b97
dsantelmo <dsantelmo@gmail.com>
2017-01-03 22:31:55 +0100

    flask routes first unit tests

--- test/node_tests.py
+++ test/node_tests.py
@@ -3,7 +3,8 @@ import os
 import unittest
 import argparse
 import mock
-from werkzeug.exceptions import InternalServerError
+import flask
+from werkzeug.exceptions import InternalServerError, NotFound
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from abrim import node
 
@@ -27,9 +28,12 @@ class NodeTestCase(unittest.TestCase):
             except OSError as error:
                 pass
 
+    #
+    # init methods
+    #
     def test_init_and_parse_without_argv(self):
         self.assertEqual((None, None, None), node._parse_args_helper())
-        self.assertRaises(InternalServerError, node._init)  # FIXME: is this a bug? no ()
+        self.assertRaises(InternalServerError, node._init)
 
     def test_init_and_parse_with_argv_without_i(self):
         sys.argv.append('-p')
@@ -47,14 +51,6 @@ class NodeTestCase(unittest.TestCase):
             node._init()
         self.assertEqual(argv_ex.exception.code, 2)
 
-    def test_parse_args_helper_with_argv(self):
-        sys.argv.append('-i')
-        sys.argv.append('-p')
-        sys.argv.append(str(self.port_to_test))
-        sys.argv.append('-l')
-        sys.argv.append('info')
-
-
     def test_init_and_parse_with_argv(self):
         sys.argv.append('-i')
         sys.argv.append('-p')
@@ -69,6 +65,36 @@ class NodeTestCase(unittest.TestCase):
         self.assertEqual(node.app.config['NODE_PORT'], self.port_to_test)
         self.assertEqual(node.app.config['USER_ID'], "the_user")
         self.assertEqual(node.app.config['NODE_ID'], "node" + str(self.port_to_test))
+
+    #
+    # flask route methods
+    #
+    def test_send_sync_get(self):
+        # @app.route('/users/<string:user_id>/nodes/<string:node_id>/items', methods=[ 'GET'])
+        with node.app.test_request_context('/users/1/nodes/1/items', method='GET') as test_req:
+            node.app.preprocess_request()
+            sys.argv.append('-i')
+            sys.argv.append('-p')
+            sys.argv.append(str(self.port_to_test))
+            node._init()
+            self.assertEqual(flask.request.path, '/users/1/nodes/1/items')
+            response = node._send_sync('1','1')
+            self.assertEqual(response.headers.get('Content-Type'), 'application/json')
+            self.assertEqual(response.status, "200 OK")
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.data, b'{\n  "items": [], \n  "status": "OK"\n}\n')
+            self.assertEqual(response.mimetype, 'application/json')
+
+    def test_send_sync_post(self):
+        # @app.route('/users/<string:user_id>/nodes/<string:node_id>/items', methods=[ 'GET'])
+        with node.app.test_request_context('/users/1/nodes/1/items', method='POST') as test_req:
+            node.app.preprocess_request()
+            sys.argv.append('-i')
+            sys.argv.append('-p')
+            sys.argv.append(str(self.port_to_test))
+            node._init()
+            self.assertEqual(flask.request.path, '/users/1/nodes/1/items')
+            self.assertRaises(NotFound, node._send_sync, '1', '1')
 
 def _main():
     unittest.main()
