64be791b9014fa4a78a3fff199ab6b6e7abcf38e
dsantelmo <dsantelmo@gmail.com>
2017-01-03 22:15:14 +0100

    stash before commit

--- git_hooks/pre-commit.sh
+++ git_hooks/pre-commit.sh
@@ -48,10 +48,10 @@ fi
 
 # http://codeinthehole.com/writing/tips-for-using-a-git-pre-commit-hook/
 
-#git stash -q --keep-index
+git stash -q --keep-index
 ./run_tests.sh
 RESULT=$?
-#git stash pop -q
+git stash pop -q
 [ $RESULT -ne 0 ] && exit 1
 
 
