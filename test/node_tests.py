f00686ff7b756dfd16b54c330bdff59499abbd5d
dsantelmo <dsantelmo@gmail.com>
2018-03-02 11:50:14 +0100

    added sys.path for travis-ci

--- test/node_tests.py
+++ test/node_tests.py
@@ -1,4 +1,8 @@
 from unittest import TestCase
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))  # FIXME use pathlib
+
 from abrim import node
 
 class TestNode(TestCase):
