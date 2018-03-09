266633e82dcdd37844fcc2926d0413adb9345a42
dsantelmo <dsantelmo@gmail.com>
2018-03-09 01:02:50 +0100

    moving comment fro testing advice

--- test/queue_in_tests.py
+++ test/queue_in_tests.py
@@ -7,6 +7,47 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))  # FIXME use path
 
 from abrim import node, queue_in
 
+
+# to test:
+#
+# import requests
+#
+# url = "http://127.0.0.1:5001/users/user_1/nodes/node_1/items/item_1"
+#
+# payload = "{\n\t\"action\": \"create_item\",\n\t\"create_date\": \"2018-01-29T21:35:15.785000+00:00\",\n\t\"client_rev\": 0\n}"
+#
+# headers = {
+#     'Content-Type': "application/json",
+#     'Cache-Control': "no-cache",
+#     'Postman-Token': "173c0991-7697-8506-bbdb-f433d4909c20"
+#     }
+#
+#
+# response = requests.request("POST", url, data=payload, headers=headers)
+# print(response.status_code)
+#
+# or:
+# import http.client
+#
+# conn = http.client.HTTPConnection("127.0.0.1:5001")
+#
+# payload = "{\n\t\"action\": \"create_item\",\n\t\"create_date\": \"2018-01-29T21:35:15.785000+00:00\",\n\t\"client_rev\": 0\n}"
+#
+# headers = {
+#     'Content-Type': "application/json",
+#     'Cache-Control': "no-cache",
+#     'Postman-Token': "0cf06ca7-5611-8aed-3739-14bde335ef00"
+#     }
+#
+#
+# conn.request("POST", "users/user_1/nodes/node_1/items/item_1", payload, headers)
+#
+# res = conn.getresponse()
+# #data = res.read()
+# #print(data.decode("utf-8"))
+# print(res.code)
+
+
 class TestNode(TestCase):
     config = None
 
