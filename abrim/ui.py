7317641113252572e2896c9c739967abc2e483ad
dsantelmo <dsantelmo@gmail.com>
2018-08-03 16:56:14 +0200

    basic log in log out

--- abrim/ui.py
+++ abrim/ui.py
@@ -2,8 +2,8 @@
 
 import traceback
 import time
-from flask import Flask, render_template, redirect, url_for
-from flask_login import LoginManager, current_user
+from flask import Flask, request, abort, render_template, redirect, url_for
+from flask_login import LoginManager, UserMixin, current_user, login_required, login_user, logout_user
 from abrim.config import Config
 from abrim.util import get_log, fragile_patch_text, resp, check_fields_in_dict, check_crc, get_crc, create_diff_edits, \
                        create_hash, args_init
@@ -11,6 +11,24 @@ from abrim.util import get_log, fragile_patch_text, resp, check_fields_in_dict, 
 log = get_log(full_debug=False)
 
 app = Flask(__name__)
+
+app.secret_key = "CHANGE_ME"  # TODO: manage this in admin UI
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "_login"
+
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        self.name = "user" + str(id)
+        self.password = self.name + "_secret"
+
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+users = [User(id) for id in range(1, 21)]
 
 
 def __end():
@@ -35,11 +53,42 @@ def _root():
         return redirect(url_for('_login'))
     return render_template('client.html')
 
-@app.route('/login', methods=['GET'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def _login():
     if  current_user.is_authenticated:
         return redirect(url_for('_root'))
-    return render_template("login.html")
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if password == username + "_secret":
+            id = username.split('user')[1]
+            user = User(id)
+            login_user(user)
+            return redirect(url_for('_root'))  # TODO: remember the origin and redirect there
+        else:
+            return abort(401)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+@login_required
+def _logout():   #TODO: check for CSRF
+    logout_user()
+    return redirect(url_for('_root'))
+
+
+@app.errorhandler(401)
+def _page_not_found(e):
+    return '<p>Login failed. <a href="' + url_for('_root') + '">Go back</a></p>'
+
+
+@login_manager.user_loader
+def _load_user(userid):
+    return User(userid)
+
 
 if __name__ == "__main__":  # pragma: no cover
     log.info("ui started")
@@ -48,8 +97,6 @@ if __name__ == "__main__":  # pragma: no cover
     # app.run(host='0.0.0.0', port=client_port, use_reloader=False)
     # app.run(host='0.0.0.0', port=client_port)
     # for pycharm debugging
-
-    login = LoginManager(app)
 
     app.run(host='0.0.0.0', port=client_port, debug=True, use_debugger=False, use_reloader=False)
     __end()
