ea83f11cba26dedcd3c178b8f38fd04c51d2412d
dsantelmo <dsantelmo@gmail.com>
2019-02-24 21:17:57 +0100

    reorder patching

--- abrim/patch.py
+++ abrim/patch.py
@@ -44,6 +44,7 @@ def _patch_server_text(config, item, other_node, n_rev, patches, text, item_crc)
 
 def process_out_patches(lock, node_id, port):
     config = Config(node_id, port)
+    # config.db.sql_debug_trace(True)
 
     there_was_nodes = False
     # to avoid one node hoarding the queue, process one patch a time for each node
@@ -51,24 +52,40 @@ def process_out_patches(lock, node_id, port):
         there_was_nodes = True
         log.debug(other_node_id)
         try:
-            item, other_node, n_rev, m_rev, patches, old_crc, new_crc = _check_first_patch(config, other_node_id)
-            if item:
-                _, text, item_crc = _get_item(config, item)
-
-                if old_crc == item_crc:
-                    # original text from client is the same as current text from server, just apply the patch and finish
-                    log.debug("CRCs match, client text and server text are the same")
-                else:
-                    log.debug("CRCs don't match, different texts")
-                sucessful_patch = _patch_server_text(config, item, other_node, n_rev, patches, text, item_crc, new_crc)
-                if sucessful_patch:
-                    log.debug("patch ok")
-                else:
-                    log.error("patch failed")
-            else:
-                log.debug("no items for this node")
-                time.sleep(15)
+            config.db.sql_debug_trace(True)
+            patch_found_item, other_node, n_rev, m_rev, patches, crc = _check_first_patch(config, other_node_id)
+            if not patch_found_item:
+                log.debug("no patch found for this item for this node")
+                time.sleep(2)
                 raise Exception("implement me! 3")
+
+            config.db.start_transaction()
+            item_found, text, item_crc = _get_item(config, patch_found_item)
+            if not item_found:
+                if n_rev == 0 and m_rev == 0:  # if revs == 0 is a new so we can create an empty one
+                    log.debug(f"creating new empty item for patch {patch_found_item}")
+                    text = ""
+                    item_crc = 1
+                    config.db.save_new_item(patch_found_item, text, item_crc)
+                else:
+                    log.debug("no patch found for this item for this node")
+                    time.sleep(2)
+                    raise Exception("implement me! 4")
+            config.db.end_transaction()
+
+            if crc == item_crc:
+                # original text from client is the same as current text from server, just apply the patch and finish
+                log.debug("CRCs match, client text and server text are the same")
+            else:
+                log.debug(f"CRCs don't match, different texts: {crc} - {item_crc}")
+
+            sucessful_patch = _patch_server_text(config, patch_found_item, other_node, n_rev, patches, text, item_crc)
+
+            if sucessful_patch:
+                log.debug("patch ok")
+            else:
+                log.error("patch failed")
+
         except TypeError:
             # log.debug("no patches")
             pass
@@ -100,4 +117,4 @@ if __name__ == '__main__':
             else:
                 # log.debug(p_name + " finished ok")
                 pass
-            time.sleep(3)  # TODO: make this adaptative
+            time.sleep(1)  # TODO: make this adaptative
